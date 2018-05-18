b1fe765a994149fac6c42c4591a6a06eb14491aa
ariel <amartorell@itba.edu.ar>
2018-05-18 17:55:03 -0300

    alan puto

--- TP9F1/TP9F1/LCDlib.cpp
+++ TP9F1/TP9F1/LCDlib.cpp
@@ -1,17 +1,59 @@
 #include "LCDlib.h"
+#include <iostream>
 
 FT_HANDLE * lcdInit(int iDevice)
 {
 	Timer timer;
-	FT_HANDLE * handle;
-	FT_STATUS status = FT_OpenEx(MY_LCD_DESCRIPTION, FT_OPEN_BY_DESCRIPTION, handle);
+	FT_HANDLE * handle=nullptr;
+	DWORD byteswritten;
+	FT_STATUS status = FT_OpenEx((PVOID)MY_LCD_DESCRIPTION, FT_OPEN_BY_DESCRIPTION, handle);
 	if (status == FT_OK)
 	{
 		status = FT_SetBitMode(handle, 0xff, 1);
 		if (status == FT_OK)
 		{
-			/*FT_Write(handle,(LCD_LOW_NIBBLE ^ )*/
+			status=FT_Write(handle, (LPVOID)((0xff) ^ LCD_D0 ^ LCD_D1 ^LCD_D2 ), 1, &byteswritten);
+			timer.start();
+			do
+			{
+				timer.stop();
+
+			} while (timer.getTime() < 4);
+			if (status == FT_OK)
+			{
+				status=FT_Write(handle, (LPVOID)((0xff) ^ LCD_D0 ^ LCD_D1 ^LCD_D2), 1, &byteswritten);
+				timer.start();
+				do
+				{
+					timer.stop();
+
+				} while (timer.getTime() < 0.1);
+				
+				status=FT_Write(handle, (LPVOID)((0xff) ^ LCD_D0 ^ LCD_D1 ^LCD_D2), 1, &byteswritten);
+				if (status == FT_OK)
+				{
+					status=FT_Write(handle, (LPVOID)((0xff) ^ LCD_D0 ^ LCD_D1 ^LCD_D2 ^ LCD_D4), 1, &byteswritten);
+					if (status == FT_OK)
+					{
+						status = FT_Write(handle, (LPVOID)((0xff) ^ LCD_D0 ^ LCD_D1 ^LCD_D2^ LCD_D4), 2, &byteswritten);
+						if (status == FT_OK)
+						{
+							status = FT_Write(handle, (LPVOID)(LCD_D3), 2, &byteswritten);
+							if (status == FT_OK)
+							{
+								status = FT_Write(handle, (LPVOID)(LCD_FUNCTION_CLEAR_DISPLAY), 2, &byteswritten);
+								if (status == FT_OK)
+								{
+									status = FT_Write(handle, (LPVOID)(LCD_D2 | LCD_D1), 2, &byteswritten);
+									std::cout << "funciono la concha de tu madre" << std::endl;
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
+	return handle;
 }
